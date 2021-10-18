9dd9a2f9d6f81ed7d794c13090bfcdcadc3084a2
okikiola11 <okikiola.apelehin@gmail.com>
2021-10-18 21:57:56 +0000

    Generate the measures model Create the measures route Create the measures migration file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :measurements
+      resources :measurements do
+        resources :measures
+      end
+
       resource :authentication, only: [:create]
       post '/signup', to: 'users#create'
     end
